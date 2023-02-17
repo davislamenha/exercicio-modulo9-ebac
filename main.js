686be8f10caacbd58c1107d1f25b12723ca32ebf
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-02-17 07:09:34 -0300

    remover tarefa e animacao

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
+let n = 0;
+
 $('form').on('submit', (e) => {
   e.preventDefault();
   adicionarTarefa();
+  ativarRemover();
 });
 
 function adicionarTarefa() {
   const tarefa = $('#tarefa-nome').val();
-  const li = $('<li></li>');
+  const li = $(`<li id=tarefa-${n}></li>`);
   const spanTarefa = $(`<span class="tarefa">${tarefa}</span>`);
-  const spanRemover = $('<span class="remover"></span>');
+  const spanRemover = $(`<span class="remover" id="${n}"></span>`);
   spanTarefa.appendTo(li);
   spanRemover.appendTo(li);
   $(li).appendTo('.lista');
+  setTimeout(() => {
+    $(li).css('display', 'flex').hide().fadeIn(500);
+    n++;
+  }, 300);
+}
+
+function ativarRemover() {
+  $('.remover').on('click', ({ target }) => {
+    const id = target.id;
+    $(`#tarefa-${id}`).fadeOut(500);
+    setTimeout(() => {
+      $(`#tarefa-${id}`).remove();
+    }, 600);
+  });
 }
